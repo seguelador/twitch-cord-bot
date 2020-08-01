81fd7f9e1ba9d042dc885752e75d3e495b99ed65
Matias Seguel <mseguelestay@gmail.com>
2020-08-01 19:00:00 -0400

    Discord-player works with play command

--- src/discord/discord.js
+++ src/discord/discord.js
@@ -1,6 +1,6 @@
 require('dotenv').config(); // Remove later when app runs from index.js
 const Discord = require('discord.js');
-const ytdl = require('ytdl-core-discord');
+const { Player } = require("discord-player");
 const { commandPrefix } = require('./config.json');
 const {
     JOIN,
@@ -10,8 +10,11 @@ const {
     PAUSE
 } = require('./constants');
 
-
+// Instantiate Discord Client
 const client = new Discord.Client();
+
+// Instantiate Discord Player
+const player = new Player(client);
 
 const token = process.env.DISCORD_BOT_TOKEN;
 
@@ -38,8 +41,9 @@ client.on('message', async (message) => {
         // Only try to join the sender's voice channel if they are in one themselves
         if (message.member.voice.channel) {
             connection = await message.member.voice.channel.join();
+            message.channel.send('Que comience la fiesta!');
         } else {
-            message.reply('You need to join a voice channel first!');
+            message.reply('Necesitas unirte a un canal de voz primero');
         }
     }
 
@@ -53,8 +57,18 @@ client.on('message', async (message) => {
 
     // Play music
     if (commandName === (commandPrefix + PLAY)) {
+        if (connection) {
+        } else {
+            message.channel.send('Para que comience la fiesta, primero debes invitarme. Escribe $join para comenzar');
+        }
         if (message.member.voice.channel) {
-            connection.play(await ytdl(content), { type: 'opus' });
+            const result = await player.play(message.member.voice.channel, content, message.member.user.tag);
+            console.log(result);
+            if(result.type === 'playlist'){
+                message.channel.send(`${result.tracks.length} canciones agregadas a la lista!\nAhora sonando: **${result.tracks[0].name}**...`);
+            } else {
+                message.channel.send(`Reproduciendo ${result.name}, agregado por ${result.requestedBy}`);
+            }
         } else {
             message.reply('You need to join a voice channel first!');
         }
